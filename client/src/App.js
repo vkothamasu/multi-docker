76a2b26c053b61ceda279c766be3a3411ccbec26
Kothamasu <vasudeva.kothamasu@gmail.com>
2020-07-10 23:27:00 +0530

    updated the app

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Docker and K8S training on Udemy</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
